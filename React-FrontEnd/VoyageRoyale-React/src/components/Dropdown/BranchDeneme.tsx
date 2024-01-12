d9d8f7847eccec697de388961545a671f06ea79d
rumeysakopuz <rumeysakpzz@gmail.com>
2024-01-12 13:43:48 +0300

    created comp

---
+++ React-FrontEnd/VoyageRoyale-React/src/components/Dropdown/BranchDeneme.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const BranchDeneme = (props: Props) => {
+  return (
+    <div>
+      Terminalden bir şeyler deniyorum arkadaşlar projeye bir şey olmaz umarım
+      :D
+    </div>
+  );
+};
