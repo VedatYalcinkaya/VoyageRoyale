5ef22f79b1763252e160ce49973c64f761e13e8e
vedat <vedatylcnky@gmail.com>
2024-01-14 00:32:52 +0300

    add UserProfileCard component

--- React-FrontEnd/VoyageRoyale-React/src/App.tsx
+++ React-FrontEnd/VoyageRoyale-React/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar/>
       <Dashboard />
       <Footer />
     </>
--- React-FrontEnd/VoyageRoyale-React/src/components/Card/UserProfileCard.tsx
+++ React-FrontEnd/VoyageRoyale-React/src/components/Card/UserProfileCard.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Typography, Grid, Box } from '@mui/material';
 
 type Props = {}
 
 const UserProfileCard = (props: Props) => {
+
   return (
-    <div>UserProfileCard</div>
-  )
+    <Box sx={{ width:"60%" , border: 1}}>
+    <Box sx={{ width: '100%' , margin: 2 , justifyContent: 'space-evenly' }}>
+      <Grid container rowSpacing={0} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{fontWeight: 'bold' }}>Name</Typography>
+          <Typography>Vedat</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{fontWeight: 'bold'}}>Surname</Typography>
+          <Typography>Yalçınkaya</Typography>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box sx={{ width: '100%' , margin: 2 , justifyContent: 'space-evenly' }}>
+      <Grid container rowSpacing={0} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{fontWeight: 'bold' }}>E-mail</Typography>
+          <Typography>vedatylcnky@gmail.com</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{fontWeight: 'bold'}}>Birth Date</Typography>
+          <Typography>01.06.1998</Typography>
+        </Grid>
+      </Grid>
+    </Box>
+    </Box>
+    
+  );
 }
 
 export default UserProfileCard
