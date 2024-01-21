e213d44b4f155769b8dd3b1c066a6d41a0a3ed4d
Tarık Direk <tarkdirek@gmail.com>
2024-01-21 15:55:16 +0300

    accept changes

--- React-FrontEnd/VoyageRoyale-React/src/store/configureStore.ts
+++ React-FrontEnd/VoyageRoyale-React/src/store/configureStore.ts
@@ -3,15 +3,11 @@ import carListSlice from './slices/CarSlices/carListSlice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import carDetailSlice from './slices/CarSlices/carDetailSlice'
 import selectPositionSlice from './slices/selectPositionSlice';
-<<<<<<< HEAD
 import carCategorySlice from './slices/CarSlices/carCategorySlice';
 import carFuelTypeSlice from './slices/CarSlices/carFuelTypeSlice';
 import carBrandTypeSlice from './slices/CarSlices/carBrandTypeSlice';
 import carGearTypeSlice from './slices/CarSlices/carGearTypeSlice';
-=======
-import carCategorySlice from './slices/carCategorySlice';
 import reservationSlice from './slices/reservationSlice';
->>>>>>> a9e7e96219a65b6b5546a2458b37981de1106398
 
 
 const vedat = "vedat";
@@ -21,15 +17,12 @@ export const store = configureStore( {
         carList:carListSlice,
         carDetail:carDetailSlice,
         positionList:selectPositionSlice,
-<<<<<<< HEAD
         carType:carCategorySlice,
         carFuelType:carFuelTypeSlice,
         carBrandType:carBrandTypeSlice,
-        carGearType:carGearTypeSlice
-=======
+        carGearType:carGearTypeSlice,
         carCategory:carCategorySlice,
         reservation: reservationSlice,
->>>>>>> a9e7e96219a65b6b5546a2458b37981de1106398
     }
 })
 
