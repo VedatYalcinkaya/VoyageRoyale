94d64be3af867620027ca2c877c2a1b1d3f16d00
ozgegul <ozgegul1@icloud.com>
2024-01-22 17:50:11 +0300

    Location Update

--- React-FrontEnd/VoyageRoyale-React/src/store/configureStore.ts
+++ React-FrontEnd/VoyageRoyale-React/src/store/configureStore.ts
@@ -1,37 +1,31 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
-import carListSlice from './slices/CarSlices/carListSlice'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import carDetailSlice from './slices/CarSlices/carDetailSlice'
-import selectPositionSlice from './slices/selectPositionSlice';
-import carCategorySlice from './slices/CarSlices/carCategorySlice';
-import carFuelTypeSlice from './slices/CarSlices/carFuelTypeSlice';
-import carBrandTypeSlice from './slices/CarSlices/carBrandTypeSlice';
-import carGearTypeSlice from './slices/CarSlices/carGearTypeSlice';
-import reservationSlice from './slices/reservationSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import carListSlice from "./slices/CarSlices/carListSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import carDetailSlice from "./slices/CarSlices/carDetailSlice";
+import selectPositionSlice from "./slices/selectPositionSlice";
+import carCategorySlice from "./slices/CarSlices/carCategorySlice";
+import carFuelTypeSlice from "./slices/CarSlices/carFuelTypeSlice";
+import carBrandTypeSlice from "./slices/CarSlices/carBrandTypeSlice";
+import carGearTypeSlice from "./slices/CarSlices/carGearTypeSlice";
+import reservationSlice from "./slices/reservationSlice";
 
+export const store = configureStore({
+  reducer: {
+    carList: carListSlice,
+    carDetail: carDetailSlice,
+    positionList: selectPositionSlice,
+    carType: carCategorySlice,
+    carFuelType: carFuelTypeSlice,
+    carBrandType: carBrandTypeSlice,
+    carGearType: carGearTypeSlice,
+    carCategory: carCategorySlice,
+    reservation: reservationSlice,
+  },
+});
 
-const vedat = "vedat";
+export const useAppDispatch: () => AppDispatch = useDispatch;
 
-export const store = configureStore( {
-    reducer:{
-        carList:carListSlice,
-        carDetail:carDetailSlice,
-        positionList:selectPositionSlice,
-        carType:carCategorySlice,
-        carFuelType:carFuelTypeSlice,
-        carBrandType:carBrandTypeSlice,
-        carGearType:carGearTypeSlice,
-        carCategory:carCategorySlice,
-        reservation: reservationSlice,
-    }
-})
-
-
-export const useAppDispatch : () => AppDispatch = useDispatch
-
-export const useAppSelector : TypedUseSelectorHook<RootState> = useSelector
-
-
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export type RootState = ReturnType<typeof store.getState>;
 
