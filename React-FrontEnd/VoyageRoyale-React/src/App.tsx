946e3b2abbba50fc737a4932abb792611b57b19e
Vedat Yalçınkaya <87366287+VedatYalcinkaya@users.noreply.github.com>
2024-01-31 02:28:25 +0300

    Update App.tsx

--- React-FrontEnd/VoyageRoyale-React/src/App.tsx
+++ React-FrontEnd/VoyageRoyale-React/src/App.tsx
@@ -3,10 +3,7 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import '@fontsource/montserrat';
 import { AuthProvider } from "./context/AuthContext";
-<<<<<<< HEAD
 
-=======
->>>>>>> a66db9399a993ecd3f224832b66f5f52148687d8
 
 const theme = createTheme({
   typography: {
