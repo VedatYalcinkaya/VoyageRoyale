3108a6a588c42da8bb44882be05048d05b03672b
vedat <vedatylcnky@gmail.com>
2024-01-31 17:10:39 +0300

    added custom update request

--- Java-Backend/VoyageRoyale-Java/src/main/java/com/tobeto/pair5/services/abstracts/CustomerService.java
+++ Java-Backend/VoyageRoyale-Java/src/main/java/com/tobeto/pair5/services/abstracts/CustomerService.java
@@ -1,6 +1,7 @@
 package com.tobeto.pair5.services.abstracts;
 
 import com.tobeto.pair5.services.dtos.customer.requests.AddCustomerRequest;
+import com.tobeto.pair5.services.dtos.customer.requests.CustomUpdateCustomerRequest;
 import com.tobeto.pair5.services.dtos.customer.requests.DeleteCustomerRequest;
 import com.tobeto.pair5.services.dtos.customer.requests.UpdateCustomerRequest;
 import com.tobeto.pair5.services.dtos.customer.responses.GetAllCustomerResponse;
@@ -14,9 +15,13 @@ public interface CustomerService {
 
     void update(UpdateCustomerRequest request);
 
+    void customUpdate(CustomUpdateCustomerRequest request);
+
     List<GetAllCustomerResponse> getAll();
 
     GetCustomerByIdResponse getById(int id);
+
+
 
 
 }
