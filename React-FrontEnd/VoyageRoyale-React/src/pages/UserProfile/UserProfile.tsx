612c7119676cbd92b8c4c3a1d0324dbb6f9f44b2
vedat <vedatylcnky@gmail.com>
2024-02-08 15:48:37 +0300

    added Customer and CorporateCustomer info based on user roles

--- React-FrontEnd/VoyageRoyale-React/src/pages/UserProfile/UserProfile.tsx
+++ React-FrontEnd/VoyageRoyale-React/src/pages/UserProfile/UserProfile.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import UserProfileCard from '../../components/Card/UserProfileCard'
 import CorporateCustomerProfileCard from '../../components/Card/CorporateCustomerProfileCard'
+import { useAppSelector } from '../../store/configureStore'
 
 type Props = {}
 
 const UserProfile = (props: Props) => {
+  const authorities:string[] | undefined = useAppSelector(state => state.getCustomerByEmail.data?.authorities)
   return (
     <div>
+       {authorities?.includes("CORPORATE_CUSTOMER") && (
       <CorporateCustomerProfileCard/>
+      )}
+      {authorities?.includes("CUSTOMER") && (
       <UserProfileCard/>
+      )}
     </div>
   )
 }
